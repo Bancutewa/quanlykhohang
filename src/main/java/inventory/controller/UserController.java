73eea5a359329f65790f6acd56118dc4ec9564de
Khai <bkhaidinh@gmail.com>
2024-12-17 03:16:27 +0700

    vid14 demo

--- src/main/java/inventory/controller/UserController.java
+++ src/main/java/inventory/controller/UserController.java
@@ -2,14 +2,19 @@ package inventory.controller;
 
 import inventory.model.User;
 import inventory.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
-
+    @GetMapping("/index")
+    public String hello() {
+        return "index";  // Trả về index.jsp
+    }
     @Autowired
     private UserService userService;
 
